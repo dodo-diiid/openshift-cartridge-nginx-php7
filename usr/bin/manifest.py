db069878b1b287223914944f7db1e633b44e6ee1
root <root@server.(none)>
2016-05-29 07:12:58 -0400

    first initial commit

--- usr/bin/manifest.py
+++ usr/bin/manifest.py
@@ -3,11 +3,12 @@
 import sys
 import yaml
 
-prefix = sys.argv[2]
+prefix = sys.argv[3]
 
 with open(sys.argv[1], 'r') as manifest_file: manifest = yaml.load(manifest_file)
 
-output  = prefix + 'NGINX_VERSION='    + manifest['Versions'][0]       + '\n';
-output += prefix + 'PHP_VERSION='      + manifest['Versions'][1];
+output  = prefix + 'CARTRIDGE_VERSION='		+ manifest['Versions'][0]	+ '\n';
+output += prefix + 'NGINX_VERSION='			+ manifest['Versions'][1]	+ '\n';
+output += prefix + 'PHP_VERSION='			+ manifest['Versions'][2];
 
 print output
